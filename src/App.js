2f6d4f09c2e79dfb074d23c04d082a3a787dd050
Tommy <tommykindle83@gmail.com>
2019-12-10 00:02:11 -0800

    Added react router and dynamic routes withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
 import Homepage from './pages/homepage.componenet'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
