792d09a6c099a7bd6e2732d42473b475f005ea96
Tommy <tommykindle83@gmail.com>
2020-05-10 00:46:50 -0700

    added header with links and svg logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./pages/homepage.componenet";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
