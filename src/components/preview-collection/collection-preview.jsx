2ca1b440c523fdee85ec01ba07bc9ecc2cce2293
Tommy <tommykindle83@gmail.com>
2020-05-12 13:43:25 -0700

    fixed type with collection-item component

--- src/components/preview-collection/collection-preview.jsx
+++ src/components/preview-collection/collection-preview.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./collection-preview.styles.scss";
-import CollectionItem from "../colletion-item/collection-item.component";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
